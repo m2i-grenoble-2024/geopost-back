63ffaa46490197f85bef589628db0720fc9af8d6
Jean Demel <demeljean@hotmail.fr>
2024-08-22 16:58:49 +0200

    image upload in base64

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -15,6 +15,19 @@ class PostController extends AbstractController {
     #[Route(methods:'POST')]
     public function add(#[MapRequestPayload] Post $post) {
         $post->setPostedAt(new \DateTimeImmutable());
+        //Upload d'image
+        if($post->getPicture() != null) {
+            //On génère un nom de fichier unique
+            $filename = uniqid().'.jpg';
+            //On décode le base64 envoyé par le front dans la requête
+            $decoded = base64_decode($post->getPicture());
+            //On le met dans un fichier accessible sur le web (en l'occurrence le dossier public)
+            file_put_contents(__DIR__.'/../../public/'.$filename, $decoded);
+            //On assigne le nom du fichier à notre post pour sauvegarder ça en base de donnée plutôt que l'image elle même
+            $post->setPicture($filename);
+        }
+
+
         $this->repo->persist($post);
         return $this->json($post, 201);    
     }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -8,12 +8,13 @@ class PostRepository {
 
     public function persist(Post $post) {
         $connection = Database::connect();
-        $query = $connection->prepare('INSERT INTO post (message, posted_at, latitude,longitude,author, category_id) VALUES(:message,:postedAt,:latitude,:longitude,:author,:categoryId)');
+        $query = $connection->prepare('INSERT INTO post (message, posted_at, latitude,longitude,author,picture, category_id) VALUES(:message,:postedAt,:latitude,:longitude,:author,:picture,:categoryId)');
         $query->bindValue(':message', $post->getMessage());
         $query->bindValue(':postedAt', $post->getPostedAt()->format('Y-m-d'));
         $query->bindValue(':latitude', $post->getLatitude());
         $query->bindValue(':longitude', $post->getLongitude());
         $query->bindValue(':author', $post->getAuthor());
+        $query->bindValue(':picture', $post->getPicture());
         if($post->getCategory() != null) {
             $query->bindValue(':categoryId',$post->getCategory()->getId());
         } else {
